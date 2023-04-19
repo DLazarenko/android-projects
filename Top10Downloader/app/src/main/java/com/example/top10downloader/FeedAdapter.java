08b4836d6413a9411250358f4278a1a8abb8aa1d
Daria <daria.lazarenko20@gmail.com>
2023-04-19 09:55:29 -0700

    getCount and getView methods added to FeedAdapter

--- Top10Downloader/app/src/main/java/com/example/top10downloader/FeedAdapter.java
+++ Top10Downloader/app/src/main/java/com/example/top10downloader/FeedAdapter.java
@@ -2,9 +2,13 @@ package com.example.top10downloader;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.util.List;
 
@@ -19,5 +23,27 @@ public class FeedAdapter extends ArrayAdapter {
         this.layoutResource = resource;
         this.layoutInflater = LayoutInflater.from(context);
         this.applications = applications;
+    }
+
+    @Override
+    public int getCount() {
+        return applications.size();
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view = layoutInflater.inflate(layoutResource, parent, false);
+        TextView tvName = view.findViewById(R.id.tvName);
+        TextView tvArtist = view.findViewById(R.id.tvArtist);
+        TextView tvSummary = view.findViewById(R.id.tvSummary);
+
+        FeedEntry currentApp = applications.get(position);
+
+        tvName.setText(currentApp.getName());
+        tvArtist.setText(currentApp.getArtist());
+        tvSummary.setText(currentApp.getSummary());
+
+        return view;
     }
 }
