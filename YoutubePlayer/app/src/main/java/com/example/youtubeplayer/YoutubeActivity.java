30a50303670431e994c60f4f6cbc235d07c2f2b8
Daria <daria.lazarenko20@gmail.com>
2023-04-24 20:02:53 -0700

    internet permission added

--- YoutubePlayer/app/src/main/java/com/example/youtubeplayer/YoutubeActivity.java
+++ YoutubePlayer/app/src/main/java/com/example/youtubeplayer/YoutubeActivity.java
@@ -4,23 +4,59 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 public class YoutubeActivity extends AppCompatActivity {
 
+    //    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        ConstraintLayout layout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_youtube, null);
+//        setContentView(layout);
+//        WebView webView = (WebView) findViewById(R.id.web_view);
+//        webView.loadUrl("file:///android_asset/page.html");
+//    }
+    private WebView webview;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onPause() {
+        super.onPause();
+        webview.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        webview.onResume();
+        super.onResume();
+    }
+
+    public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
 
         ConstraintLayout layout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_youtube, null);
         setContentView(layout);
-        WebView webView = (WebView) findViewById(R.id.web_view);
-        webView.loadUrl("file:///android_asset/page.html");
+        webview = new WebView(this);
+        setContentView(webview);
+
+        final WebSettings settings = webview.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);
+        settings.setPluginState(WebSettings.PluginState.ON);
+        settings.setLoadWithOverviewMode(true);
+        settings.setUseWideViewPort(true);
+
+        webview.setWebChromeClient(new WebChromeClient());
+        webview.setPadding(0, 0, 0, 0);
+
+        webview.loadUrl("file:///android_asset/page.html");
+    }
 
 //        Button button1 = new Button(this);
 //        button1.setLayoutParams(new ConstraintLayout.LayoutParams(300,80));
 //        button1.setText("Button added");
 //        layout.addView(button1);
 
-    }
 }
