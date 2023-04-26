c24a6c8b9ec87eec84665a2ad643b7742f7687ee
Daria <daria.lazarenko20@gmail.com>
2023-04-25 17:14:05 -0700

    Standalone Activity added

---
+++ YoutubePlayer/app/src/main/java/com/example/youtubeplayer/StandaloneActivity.java
@@ -0,0 +1,29 @@
+package com.example.youtubeplayer;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StandaloneActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_standalone);
+
+        Button btnPlayVideo = (Button) findViewById(R.id.btnPlayVideo);
+        Button btnPlaylist = (Button) findViewById(R.id.btnPlayList);
+
+        btnPlayVideo.setOnClickListener(this);
+        btnPlaylist.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
--- YoutubePlayer/app/src/main/java/com/example/youtubeplayer/YoutubeActivity.java
+++ YoutubePlayer/app/src/main/java/com/example/youtubeplayer/YoutubeActivity.java
@@ -1,23 +1,15 @@
 package com.example.youtubeplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
+
 public class YoutubeActivity extends AppCompatActivity {
 
-    //    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        ConstraintLayout layout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_youtube, null);
-//        setContentView(layout);
-//        WebView webView = (WebView) findViewById(R.id.web_view);
-//        webView.loadUrl("file:///android_asset/page.html");
-//    }
     private WebView webview;
 
     @Override
@@ -50,10 +42,5 @@ public class YoutubeActivity extends AppCompatActivity {
 
         webview.loadUrl("file:///android_asset/page.html");
     }
-
-//        Button button1 = new Button(this);
-//        button1.setLayoutParams(new ConstraintLayout.LayoutParams(300,80));
-//        button1.setText("Button added");
-//        layout.addView(button1);
 
 }
