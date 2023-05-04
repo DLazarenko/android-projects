c82564751db9c3d7e2fc51ed81e7c39a951afb57
Daria <daria.lazarenko20@gmail.com>
2023-05-03 23:14:16 -0700

    added PhotoDetail and Search activities

---
+++ FlickrBrowser/app/src/main/java/com/example/flickrbrowser/SearchActivity.java
@@ -0,0 +1,28 @@
+package com.example.flickrbrowser;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
